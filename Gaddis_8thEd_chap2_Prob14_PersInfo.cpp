8e1671cb515cdf1c729e37e85a820f2b8c79401e
ChrisNakahara <cnakahara1@student.rccd.edu>
2026-03-05 12:17:51 -0800

    Prob 14

---
+++ Gaddis_8thEd_chap2_Prob14_PersInfo.cpp
@@ -0,0 +1,10 @@
+//Christopher Nakahara
+//March 05 2026 11:57am
+//Miles Per Gallon
+#include <iostream>
+using namespace std;
+int main () {
+    cout << "Chris Nakahara" << endl << "1177 W Linden St apt2, Riverside, Ca , 92507" << endl
+    << "(424)316-9694" << endl << "Computer Science" << endl;
+    return 0;
+}
