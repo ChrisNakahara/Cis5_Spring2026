455cfb68d34479fdbe0e1d2a361d0fb516dbb72c
ChrisNakahara <cnakahara1@student.rccd.edu>
2026-03-05 11:39:31 -0800

    finished prob 3

--- Gaddis_8thEd_chap2_Prob3_SalesTax.cpp
+++ Gaddis_8thEd_chap2_Prob3_SalesTax.cpp
@@ -5,9 +5,10 @@
 using namespace std;
 int main ()
 {
-    int tax;
-
-
-    
+    int bill = 95;
+    float sTax = 1.04, cTax = 1.02, total;
+    total = bill * sTax * cTax;
+    cout << "The total on your $" << bill << " bill after tax is " << total << endl;
+  
     return 0;
 }
