11eba3a6184491bcd4d9735af7ec2d8b0f9851f7
ChrisNakahara <cnakahara1@student.rccd.edu>
2026-03-05 11:51:31 -0800

    prob 1

---
+++ Gaddis_8thEd_chap2_Prob1_Sumof2Nums.cpp
@@ -0,0 +1,12 @@
+//Christopher Nakahara
+//March 05 2026 11:41am
+//Sum of two Numbers
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int fNum = 50, sNum = 100, total;
+    total = fNum + sNum;
+    cout << "the total of the two numbers is " << total << endl;
+    return 0;
+}
