4c726c8437252479b796d06f76b6e76f65f0309c
ChrisNakahara <cnakahara1@student.rccd.edu>
2026-03-05 12:07:30 -0800

    Prob 10

---
+++ Gaddis_8thEd_chap2_Prob10_MPG.cpp
@@ -0,0 +1,12 @@
+//Christopher Nakahara
+//March 03 2026 11:57am
+//Miles Per Gallon
+#include <iostream>
+using namespace std;
+int main () {
+    int gallons = 15, miles = 375, MPG;
+    MPG = miles / gallons;
+    cout << "A car that has a " << gallons << " gallon tank and can drive " << 
+    miles << " miles per tanks has a mpg rate of " << MPG << endl;
+    return 0;
+}
