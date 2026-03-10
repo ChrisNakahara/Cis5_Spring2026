75d6b604e49ae40eeb1accfc428df6ba4f8145e5
Christopher Nakahara <cnakahara1>
2026-03-09 23:28:33 -0700

    problem 8

--- Gaddis_8thEd_chap2_Prob8_TotPurchase.cpp
+++ Gaddis_8thEd_chap2_Prob8_TotPurchase.cpp
@@ -2,12 +2,19 @@
 //March 05 2026 12:20pm
 //Miles Per Gallon
 #include <iostream>
+#include <iomanip>
 using namespace std;
 int main () 
 {
     float item1 = 15.95, item2 = 24.95, item3 = 6.95, item4 = 12.95, item5 = 3.95, 
-    subTot, tax = .07, total;
-    subTot = item1 + item2 + item3 + item4 + item5;
+    subTot, tax, total; // declaring variables 
+    subTot = item1 + item2 + item3 + item4 + item5; // adding all items together to get the subtotal
+    tax = subTot * .07; // calculating tax 
+    total = tax + subTot; // calculating total 
+
+    cout << "$" << item1 << endl << "$" << item2 << endl << "$" << item3 << endl << 
+    "$" << item4 << endl << "$" << item5 << endl <<"Subtotal: $" << subTot << endl <<
+     "Tax: $" << fixed << setprecision(2)<< tax << endl << "Total :$" << total ;
     
     return 0;
 }
