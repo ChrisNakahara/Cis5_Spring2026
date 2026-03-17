091310ebbb7194e5c701c903a145003a135ed8f5
Christopher Nakahara <cnakahara1>
2026-03-16 22:39:39 -0700

    hw

---
+++ Savitch_9thEd_Chap2_Prob9_Sums.cpp
@@ -0,0 +1,35 @@
+/* 
+ * File:   main.cpp
+ * Author: Dr. Mark E. Lehr
+ * Created on January 2, 2019, 12:36 PM
+ * Purpose:  Creation of Template to be used for all
+ *           future projects
+ */
+
+//System Libraries
+#include <iostream>  //Input/Output Library
+using namespace std;
+
+//User Libraries
+
+//Global Constants, no Global Variables are allowed
+//Math/Physics/Conversions/Higher Dimensions - i.e. PI, e, etc...
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Set the random number seed
+    
+    //Declare Variables
+    int allTot = 0, posTot = 0,negTot = 0, num1,num2,num3,num4,num5,num6,num7,num8,num9,num10;
+    //Initialize or input i.e. set variable values
+    cout << "Input 10 numbers, any order, positive or negative" << endl;
+    cin >> num1 >> num2 >> num3 >> num4 >> num5 >> num6 >> num7 >> num8 >> num9 >> num10;
+    //Map inputs -> outputs
+     allTot = num1 + num2;
+    //Display the outputs
+
+    //Exit stage right or left!
+    return 0;
+}
