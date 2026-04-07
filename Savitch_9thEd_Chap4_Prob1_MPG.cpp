e5c606dbf8a175a61555389e9e507895a2eb1158
ChrisNakahara <cnakahara1@student.rccd.edu>
2026-04-07 12:41:11 -0700

    do at home

---
+++ Savitch_9thEd_Chap4_Prob1_MPG.cpp
@@ -0,0 +1,35 @@
+/* 
+ * File:   
+ * Author: 
+ * Created on 
+ * Purpose:  Creation of Template to be used for all
+ *           future projects
+ */
+
+//System Libraries
+#include <iostream>  //Input/Output Library
+using namespace std;
+
+//User Libraries
+
+//Global Constants, no Global Variables are allowed
+//Math/Physics/Conversions/Higher Dimensions - i.e. PI, e, etc...
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Set the random number seed
+    
+    //Declare Variables
+    
+    //Initialize or input i.e. set variable values
+    
+    //Map inputs -> outputs
+    cout << "Enter number of liters of gasoline:" << endl << endl;
+    cout << "Enter·number·of·miles·traveled:" << endl << endl;
+    //Display the outputs
+
+    //Exit stage right or left!
+    return 0;
+}
